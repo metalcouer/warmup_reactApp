1a422d3855af571fb3b587fe794c50ec4cd7474c
Diane <metalcouer@gmail.com>
2018-12-17 09:57:46 -0700

    first commit, added html &app js

---
+++ app.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+
+   
+    render() {
+        
+        return (
+            // <ul>
+            //   {this.props.todos.map((item, idx) => {
+            //     return <ListItem
+            //       key={idx}
+            //       todo={item.todo}
+            //       time={item.time}
+            //     />
+            //   }
+            //   )}
+            // </ul>
+        )
+    }
+}
+
+export default List
